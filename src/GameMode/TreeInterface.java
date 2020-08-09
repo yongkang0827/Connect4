8493b10a3d8cc4a8021dd539a72cfd8024842163
yongkang0827 <tsuna.kang838@gmail.com>
2020-08-09 23:05:50 +0800

    yk push

---
+++ src/GameMode/TreeInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public interface TreeInterface<T> {
+    public void add(T newEntry);
+    public void delete();
+    public void root();//return root node
+    public boolean isEmpty();
+    //to be continue
+}
